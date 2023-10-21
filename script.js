6b991525362113a2ddc46e82f2b2dc7845cd6e87
joerow <joerow@gmail.com>
2023-10-21 22:13:09 +0800

    add factorial

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ function power(base, exponent) {
   if (exponent == 0) return 1;
   return base * power(base, exponent - 1);
 }
+
+function factorial(number) {
+  if (number == 1) return number;
+  return number * factorial(number - 1);
+}
